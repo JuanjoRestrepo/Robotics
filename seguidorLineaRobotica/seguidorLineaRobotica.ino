9fc8d532cc6dbe4ce5e4cf24f3c486d3b3f80a1f
juanjorestrepo <49797614+JuanjoRestrepo@users.noreply.github.com>
2023-05-13 15:19:48 -0500

    Codigo Seguidor de Linea Defininito
    Ya sigue la linea bien, falta implementarle la logica de detectar objetos y rodearlos

--- seguidorLineaRobotica/seguidorLineaRobotica.ino
+++ seguidorLineaRobotica/seguidorLineaRobotica.ino
@@ -3,129 +3,70 @@ int Echo = A4;
 int Trig = A5;  
 int maxDist = 15;
 #define servo A0
-
-#define L_S A1 //ir sensor Left
-#define C_S A2 //ir sensor center
-#define R_S A3 //ir sensor Right
-
-//#define LT1 digitalRead(11) //left
-//#define LT2 digitalRead(4)
-//#define LT3 digitalRead(2) //right
-
 NewPing sonar (Trig, Echo, maxDist);
-
-
-int ENA = 10; 
-int IN1 = 9;
-int IN2 = 8;
-int IN3 = 7;
-int IN4 = 6;
-int ENB = 5;
-
-/* LLANTAS IZQUIERDA 
- * IN1 
- * IN2 
- * 
- * LLANTAS DERECHA
- * IN3
- * IN4
- * 
- */
-
-// VELOCIDAD Duty Cycle de los motores 0-255
-int velocidad = 80; //120;//150 
-
-// DISTANCIAS
+ 
+//Line Tracking IO define
+#define LT_R digitalRead(11)
+#define LT_M digitalRead(4)
+#define LT_L digitalRead(2)
+ 
+#define ENA 10
+#define ENB 5
+#define IN1 9
+#define IN2 8
+#define IN3 7
+#define IN4 6
+ 
+#define velocidad 100//120 //150
 int distanciaDerecha = 0, distanciaIzquierda = 0, distanciaMitad = 0;
-
-void setup()
-{
-
-  Serial.begin(9600);
-
-  pinMode(R_S, INPUT); // declare if sensor as input 
-  pinMode(C_S, INPUT); // declare ir sensor as input 
-  pinMode(L_S, INPUT); // declare ir sensor as input
-  
-  pinMode(Echo, INPUT);    
-  pinMode(Trig, OUTPUT); 
-    
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(ENB,OUTPUT);
-  
-  pinMode(servo, OUTPUT);
-
-  //servoPulse(servo, 90);
-  moveServo();
-
-  distanciaMitad = medirDistancia();
-  delay(500);
+ 
+void forward(){
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  digitalWrite(IN1, LOW); //HIGH ORIGINAL
+  digitalWrite(IN2, HIGH);  //LOW ORIGINAL
+  digitalWrite(IN3, LOW);  //LOW ORIGINAL
+  digitalWrite(IN4, HIGH); //HIGH ORIGINAL
+  Serial.println("go forward!");
+}
+ 
+void back(){
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  digitalWrite(IN1, LOW); //LOW ORIGINAL
+  digitalWrite(IN2, HIGH); //HIGH ORIGINAL
+  digitalWrite(IN3, HIGH); //HIGH ORIGINAL
+  digitalWrite(IN4, LOW); //LOW ORIGINAL
+  Serial.println("go back!");
+}
+ 
+void right(){
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW); //HIGH ORIGINAL
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go left!");
+}
+ 
+void left(){
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  digitalWrite(IN1, LOW); //HIGH ORIGINAL
+  digitalWrite(IN2, HIGH); //LOW ORIGINAL
+  digitalWrite(IN3, LOW); //HIGH ORIGINAL
+  digitalWrite(IN4, LOW); //LOW ORIGINAL
+  Serial.println("go right!");
+} 
+ 
+void Stop(){
+   digitalWrite(ENA, LOW);
+   digitalWrite(ENB, LOW);
+   Serial.println("Stop!");
 }
 
-void loop()
-{
-  distanciaMitad = medirDistancia();
-  //Serial.print("Distancia Mitad: ");
-  //Serial.println(distanciaMitad);
-
-
-  /*
-
-  // Si el sensor derecho y el sensor izquierdo están en color blanco, 
-  // Sigue adelante moveForward
-  if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0))
-  {
-    if(distanciaMitad > maxDist)
-    {
-      moveForward();
-    }
-    else
-    {
-      checkSide();
-    }
-  }
-  //si el sensor derecho es negro y el sensor izquierdo es blanco, 
-  // llamará a la función turnRight
-  else if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 0))
-  {
-    turnRight();
-  }  
-  
-  //si el sensor derecho es blanco y el sensor izquierdo es negro
-  //llamará a la función turnLeft
-  else if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 1))
-  {
-    turnLeft();
-  } */
-
-  if(digitalRead(C_S)){
-    moveForward();
-    delay(1000);
-    Serial.print("IR Centro: ");
-    Serial.println(digitalRead(C_S));
-  }
-  else if(digitalRead(L_S) ) { 
-    turnLeft();
-    while(digitalRead(L_S));
-    delay(1000);
-    Serial.print("IR Izquierda: ");
-    Serial.println(digitalRead(L_S));                             
-  }   
-  else if(digitalRead(R_S)) {
-    turnRight();
-    while(digitalRead(R_S));
-    delay(1000);
-    Serial.print("IR Derecha: ");  
-    Serial.println(digitalRead(R_S));
-  }
-  delay(10);
-}
-
-
+//  ------------------ SERVO --------------------
 
 // Enviar Pulsos PWM al servo para moverlo
 void servoPulse(int pin, int angle)
@@ -136,28 +77,26 @@ void servoPulse(int pin, int angle)
   digitalWrite(pin, LOW);
   delay(50); // Refresh cycle of servo
 }
-
 void moveServo()
 {
   //Rango Servo: 10°- 180° 
-  for (int angle = 90; angle <= 140; angle += 5)  
+  for (int angle = 130; angle <= 180; angle += 5)  
   {
     servoPulse(servo, angle);  
   }
   
-  for (int angle = 140; angle >= 40; angle -= 5)  
+  for (int angle = 180; angle >= 60; angle -= 5)  
   {
     servoPulse(servo, angle);  
   }
   
-  for (int angle = 40; angle <= 90; angle += 5)  
+  for (int angle = 60; angle <= 130; angle += 5)  
   {
     servoPulse(servo, angle);  
   }
 }
 
-
-// ULTRASONIDO
+// ------------------ ULTRASONIDO -----------------
 int medirDistancia()   
 {
   //return sonar.ping_cm();
@@ -169,129 +108,42 @@ int medirDistancia()
   return time / 29 / 2;  
 } 
 
-// Mover Motores
-void moveForward()
-{
-  analogWrite(ENA,velocidad);
-  analogWrite(ENB,velocidad);  
-  digitalWrite(IN1,LOW);      
-  digitalWrite(IN2,HIGH);    // llantas derecha hacia adelante (forward)
 
-  digitalWrite(IN3,LOW);     
-  digitalWrite(IN4,HIGH);    // llantas izquierda hacia adelante (forward)
-}
-
-void moveBackwards()
-{
-  analogWrite(ENA,velocidad);
-  analogWrite(ENB,velocidad);
-  digitalWrite(IN1,HIGH);      
-  digitalWrite(IN2,LOW);    // llantas derecha hacia atras (backwards)
-  
-  digitalWrite(IN3,HIGH);     
-  digitalWrite(IN4,LOW);    // llantas izquierda hacia atras (backwards)
-
-}
-
-void turnRight()
-{
-  analogWrite(ENA,velocidad);
-  analogWrite(ENB,velocidad);
-  digitalWrite(IN1, LOW); 
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW); 
-  digitalWrite(IN4, LOW); 
-}
-
-void turnLeft()
-{
-  analogWrite(ENA,velocidad);
-  analogWrite(ENB,velocidad);
-  digitalWrite(IN1, LOW); 
-  digitalWrite(IN2, LOW); 
-  digitalWrite(IN3, LOW); 
-  digitalWrite(IN4, HIGH); 
-}
-
-void Stop()
-{
-  analogWrite(ENA, LOW);
-  analogWrite(ENB, LOW);
-} 
-
-// Chequear Lados y logica
-void checkSide(){
-    Stop();
-    delay(100);
-    for (int angle = 90; angle <= 150; angle += 5)  {
-      servoPulse(servo, angle);  }
-      delay(300);
-      distanciaIzquierda = medirDistancia();
-      Serial.print("Dist Izquierda: ");
-      Serial.println(distanciaIzquierda);
-      delay(100);
-    for (int angle = 150; angle >= 30; angle -= 5)  {
-      servoPulse(servo, angle);  }
-      delay(500);
-      distanciaDerecha = medirDistancia();
-      Serial.print("Dist Derecha: ");
-      Serial.println(distanciaDerecha);
-      delay(100);
-    for (int angle = 30; angle <= 90; angle += 5)  {
-      servoPulse(servo, angle);  }
-      delay(300);
-      compareDistance(distanciaDerecha, distanciaIzquierda);
-}
-
-void compareDistance(int distDer, int distIzq){
-  if(distIzq > distDer)
-  {
-    turnLeft();
-    delay(500);
-    moveForward();
-    delay(600);
-    turnRight();
-    delay(500);
-    moveForward();
-    delay(600);
-    turnRight();
-    delay(400);
+void followLine() {
+  if(LT_M){
+    forward();
+    Serial.println("Centro Encedido");
   }
-  else
-  {
-    turnRight();
-    delay(500);
-    moveForward();
-    delay(600);
-    turnLeft();
-    delay(500);
-    moveForward();
-    delay(600);  
-    turnLeft();
-    delay(400);
+  else if(LT_R) { 
+    right();
+    Serial.println("Derecha Encedido");
+    while(LT_R);                             
+  }   
+  else if(LT_L) {
+    left();
+    Serial.println("Izquierda Encedido");
+    while(LT_L);  
   }
 }
 
-void Run()
-{
-  Stop();
-  delay(3000);
-  distanciaMitad = medirDistancia();
-  Serial.print("Distancia Mitad: ");
-  Serial.println(distanciaMitad);
-  delay(100);
-  moveForward();
-  if (distanciaMitad <= 10)
-  {
-    Stop();
-    delay(100);
-    moveBackwards();
-    delay(1500);
-    Stop();
-  }
-  else
-  {
-    moveForward();
-    delay(5000);
-  }
+
+// ------------------ SETUP and MAIN LOOP -----------------
+ 
+void setup(){
+  Serial.begin(9600);
+  pinMode(LT_R,INPUT);
+  pinMode(LT_M,INPUT);
+  pinMode(LT_L,INPUT);
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);
+  pinMode(servo, OUTPUT);
+
+  moveServo();
+  //distanciaMitad = medirDistancia();
+  //delay(500);
+}
+ 
+
+void loop(){
+  followLine();
 }
