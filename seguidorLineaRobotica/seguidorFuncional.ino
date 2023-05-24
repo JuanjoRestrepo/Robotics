643e889e1fb86a98c6aaf3e0961097f0283ec11b
juanjorestrepo <49797614+JuanjoRestrepo@users.noreply.github.com>
2023-05-24 10:18:47 -0500

    Vuelvo a corregir error del seguidorFuncional.ino
    Habia fallado, ya no seguia la linea al haber intentado implementar la funcion de evadir obstaculos con el servo y el ultrasonido

--- seguidorLineaRobotica/seguidorFuncional.ino
+++ seguidorLineaRobotica/seguidorFuncional.ino
@@ -1,72 +1,72 @@
-
- //Line Tracking IO define
+//Line Tracking IO define
 #define LT_R digitalRead(11)
 #define LT_M digitalRead(4)
 #define LT_L digitalRead(2)
- 
+
 #define ENA 10
 #define ENB 5
 #define IN1 9
 #define IN2 8
 #define IN3 7
 #define IN4 6
- 
-#define carSpeed 80//120 //150
- 
+
+#define carSpeed 95
+//90 //100 //120 //150
+
 void forward(){
-  analogWrite(ENA, carSpeed);
-  analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  Serial.println("go forward!");
-}
- 
-void back(){
   analogWrite(ENA, carSpeed);
   analogWrite(ENB, carSpeed);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go forward!");
+}
+
+void back(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
   Serial.println("go back!");
 }
- 
+
 void left(){
   analogWrite(ENA, carSpeed);
   analogWrite(ENB, carSpeed);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  digitalWrite(IN4, LOW);
   Serial.println("go left!");
 }
- 
+
 void right(){
   analogWrite(ENA, carSpeed);
   analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, HIGH);
+  digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW); 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
   Serial.println("go right!");
 } 
- 
+
 void stop(){
    digitalWrite(ENA, LOW);
    digitalWrite(ENB, LOW);
    Serial.println("Stop!");
 } 
- 
+
 void setup(){
   Serial.begin(9600);
   pinMode(LT_R,INPUT);
   pinMode(LT_M,INPUT);
   pinMode(LT_L,INPUT);
 }
- 
-void loop() {
+
+void seguirLinea() {
   if(LT_M){
     forward();
     Serial.println("Centro Encedido");
@@ -81,4 +81,12 @@ void loop() {
     Serial.println("Izquierda Encedido");
     while(LT_L);  
   }
+}
+
+void loop(){
+  //right(); YA
+  //left(); YA
+  // forward(); YA
+  // back(); YA
+  seguirLinea();
 }
